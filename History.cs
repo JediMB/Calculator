2ab5c0bf5b8555f283c39ae8b3a63b9376956200
M Berntson <112472542+JediMB@users.noreply.github.com>
2022-09-28 16:44:31 +0200

    Kodat det mesta av den primära loopen. Läste felt i instruktionerna och gjorde den lite mer komplex än vad som behövdes...

--- History.cs
+++ History.cs
@@ -1,28 +1,80 @@
 ﻿namespace Calculator
 {
-    struct History
+    public enum Operation
     {
-        enum Operation
-        {
-            Add,
-            Subtract,
-            Multiply,
-            Divide
-        }
+        Null,
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+
+    struct Calculation
+    {
 
         private Queue<decimal> numbers;
         private Queue<Operation> operations;
         private decimal result;
 
-        public History()
+        public Calculation()
         {
             numbers = new Queue<decimal>();
             operations = new Queue<Operation>();
             result = 0;
         }
 
-        // void AddOperation(decimal number, Operation operation) - lägger till ett nummer/operation sist i kön
-        // string GetCalculation() - returnerar en sträng som visar alla nummer och operationer i sekvens
+        public bool AddOperation(decimal number, Operation operation)
+        {
+            if (number == 0 && operation == Operation.Divide)
+            {
+                Console.WriteLine("Can't divide by zero!");
+                return false;
+            }
+
+            numbers.Enqueue(number);
+            operations.Enqueue(operation);
+
+            return true;
+        }
+
+        private char GetOperation(Operation operation)
+        {
+            if (operation == Operation.Add)
+                return '+';
+            if (operation == Operation.Subtract)
+                return '-';
+            if (operation == Operation.Multiply)
+                return '×';
+            if (operation == Operation.Divide)
+                return '÷';
+            else return '?';
+        }
+
+        public string GetCalculation()
+        {
+            // Make sure the queues don't somehow have different lengths
+            if (numbers.Count == operations.Count)
+            {
+                if (numbers.Count == 0)
+                    return "(no values)";
+
+                decimal[] numberArray = numbers.ToArray();
+                Operation[] operationArray = operations.ToArray();
+
+                string calculation = numberArray[0].ToString();
+
+                for (int i = 1; i < numberArray.Length; i++)
+                {
+                    calculation += $" {GetOperation(operationArray[i])} {numberArray[i]}";
+                }
+
+                return calculation;
+            }
+            else
+            {
+                throw new Exception("Queue members in a history instance somehow have different sizes!");
+            }
+        }
         // decimal GetResult() - returnerar resultatet
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,149 @@
-﻿namespace Calculator
+﻿using System.Data;
+
+namespace Calculator
 {
     internal class Program
     {
         static void Main(/*string[] args*/)
         {
+            // En lista för att spara historik för räkningar (History.cs -> enum Operation -> struct History (Queue<Decimal>, Queue<Operation>))
+            Queue<Calculation> calculations = new Queue<Calculation>();
+            bool exit = false;
+
             // Välkomnande meddelande (cw)
-            // En lista för att spara historik för räkningar (History.cs -> enum Operation -> struct History (Queue<Decimal>, Queue<Operation>)) 
-            // Användaren matar in tal och matematiska operation
-            // OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+            Console.WriteLine("Welcome to Group 8's calculator!");
+
+
+            while (!exit)
+            {
+                Console.WriteLine("What would you like to do?" +
+                    "\n1. Make a calculation" +
+                    "\n2. Show calulation history" +
+                    "\n0. Exit the calculator");
+
+                Console.Write("\n:");
+                char menuSelection = Console.ReadKey().KeyChar;
+                
+                switch (menuSelection)
+                {
+                    case '0':
+                        exit = true;
+                        break;
+
+                    case '1':
+                        Calculation(calculations);
+                        break;
+
+                    case '2':
+                        ShowHistory();
+                        break;
+
+                    default:
+                        Console.Clear();
+                        break;
+                }
+                
+            }
             // Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
             // Lägga resultat till listan
             // Visa resultat
             // Fråga användaren om den vill visa tidigare resultat.
             // Visa tidigare resultat
             // Fråga användaren om den vill avsluta eller fortsätta
-            //Try - catch!
+            Console.Write("\n\nPress any key to exit... ");
+            Console.ReadKey();
+        }
+
+        static void Calculation(Queue<Calculation> calculations)
+        {
+            Calculation calculation = new();
+            decimal result;
+
+            Console.WriteLine("Let's count up your crimes!");
+
+            // Användaren matar in tal och matematiska operation
+
+            while (true)
+            {
+                // OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+                Console.Write("\nInput your first number: ");
+                if (!decimal.TryParse(Console.ReadLine(), out decimal number))
+                {
+                    Console.WriteLine("ERROR: NaN (not a number)");
+                    continue;
+                }
+
+                calculation.AddOperation(number, Operation.Null);
+                result = number;
+                break;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("What operation do you want to perform?" +
+                    "\n1. Addition (+)" +
+                    "\n2. Subtraction (-)" +
+                    "\n3. Multiplication (×)" +
+                    "\n4. Division (÷)" +
+                    "\n0. Nothing!");
+                Console.Write(": ");
+
+
+                if (!int.TryParse(Console.ReadKey().KeyChar.ToString(), out int operationSelection) || operationSelection > 4)
+                {
+                    Console.WriteLine("ERROR: Invalid input!");
+                    continue;
+                }
+
+                if (operationSelection == 0)
+                    break;
+
+                Operation operation = (Operation)operationSelection;
+
+                if (operation == Operation.Add)
+                    Console.WriteLine("What number do you want to add?");
+                else if (operation == Operation.Subtract)
+                    Console.WriteLine("What number do you want to subtract by?");
+                else if (operation == Operation.Multiply)
+                    Console.WriteLine("What number do you want to multiply with?");
+                else if (operation == Operation.Divide)
+                    Console.WriteLine("What number do you want to divide by?");
+
+                while (true)
+                {
+                    Console.Write(": ");
+                    if (!decimal.TryParse(Console.ReadLine(), out decimal number))
+                    {
+                        Console.WriteLine("ERROR! NaN (not a number)");
+                        continue;
+                    }
+
+                    if (number == 0 && operation == Operation.Divide)
+                    {
+                        Console.WriteLine("ERROR: Can't divide by zero");
+                    }
+
+                    switch (operation) // TODO: CONTINUE HERE!!
+                    {
+                        case Operation.Add:
+                            break;
+                        case Operation.Subtract:
+                            break;
+                        case Operation.Multiply:
+                            break;
+                        case Operation.Divide:
+                            break;
+                    }
+                    
+                    break;
+                }
+
+            }
+        }
+
+        static void ShowHistory()
+        {
+
         }
     }
 }
